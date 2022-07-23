3f689e56dfd622c4cf319576ed146e429969852c
Eric Laursen <ericjlaursen@gmail.com>
2022-07-22 22:58:56 -0400

    init

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-module.exports = {
-  createComponent: () => `a string here`
+import inquirer  from 'inquirer';
+
+const startingQuestions = [
+  {
+    type: 'input',
+    name: 'componentName',
+    message: 'what is the name of the component?',
+  },
+];
+
+async function doWork() {
+  let initialAnswers = await inquirer.prompt(startingQuestions);
+  console.log({
+    initialAnswers,
+  });
+  
 }
+
+doWork()
+
+// module.exports = {
+//   doWork
+// }
+// see "a-cli" for inquirer example
