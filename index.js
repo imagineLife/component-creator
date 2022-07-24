05f08f1e44236689bf883658a97af21c934dd7b1
Eric Laursen <ericjlaursen@gmail.com>
2022-07-24 08:41:00 -0400

    chore(change export syntax): change commitlint export syntax
    default to module.exports

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ async function createTheComponent() {
   let { componentName } = await inquirer.prompt(startingQuestions); 
   const cased = componentName.charAt(0).toUpperCase() + componentName.slice(1);
   console.log(`creating directory ${cased} in ${process.cwd()}`)
-  
-  
 }
 
 createTheComponent();
