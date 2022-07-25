203027d7fe8d0de8701e4310693005f0282df7dd
Eric Laursen <ericjlaursen@gmail.com>
2022-07-25 09:26:02 -0400

    style(apply formatting): run formatting
    format the files
    
    re #1

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import inquirer  from 'inquirer';
+const inquirer = require("inquirer");
 
 const startingQuestions = [
   {
-    type: 'input',
-    name: 'componentName',
-    message: 'what is the name of the component?',
+    type: "input",
+    name: "componentName",
+    message: "what is the name of the component?",
   },
 ];
 
 async function createTheComponent() {
-  let { componentName } = await inquirer.prompt(startingQuestions); 
+  const { componentName } = await inquirer.prompt(startingQuestions);
   const cased = componentName.charAt(0).toUpperCase() + componentName.slice(1);
-  console.log(`creating directory ${cased} in ${process.cwd()}`)
+  console.log(`creating directory ${cased} in ${process.cwd()}`);
 }
 
 createTheComponent();
