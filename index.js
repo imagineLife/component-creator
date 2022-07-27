00094f9243de11828034d4db207fb7b95801cca3
Eric Laursen <ericjlaursen@gmail.com>
2022-07-27 15:56:15 -0400

    feat(creates component files): creates some basic files
    Creates a ComponentName.js, index.css, and ComponentName.spec.js - including skeletal react
    component content and enzyme test content
    
    re #3

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const inquirer = require("inquirer");
-const { mkdir } = require("fs").promises;
-const { capitalizeFirst, createComponentDirPath } = require("./helpers");
+const { mkdir, open, appendFile } = require("fs").promises;
+const {
+  capitalizeFirst,
+  createComponentDirPath,
+  componentIndexString,
+  componentTestFileString,
+} = require("./helpers");
 
 const startingQuestions = [
   {
@@ -14,14 +19,47 @@ async function createComponent() {
   try {
     const { componentName } = await inquirer.prompt(startingQuestions);
     const formattedComponentName = capitalizeFirst(componentName);
-    console.log(`Creating ${formattedComponentName} now...`);
 
+    console.log(`Creating ${formattedComponentName} dir...`);
     const componentDirPath = createComponentDirPath({
       componentName: formattedComponentName,
       cwd: process.cwd(),
     });
-
     await mkdir(componentDirPath);
+    console.log(`SUCCESS! now creating index.js file`);
+
+    /*
+      create index.js file
+    */
+    const indexContent = componentIndexString({
+      componentName: formattedComponentName,
+    });
+    const fileDescriptor = await open(`${componentDirPath}/index.js`, "a");
+    await appendFile(fileDescriptor, `${indexContent}\n`);
+    await fileDescriptor.close();
+
+    /*
+      create index.css file
+    */
+    const cssFileDescriptor = await open(
+      `${componentDirPath}/${formattedComponentName}.css`,
+      "a"
+    );
+    await cssFileDescriptor.close();
+
+    /*
+      create Component.spec.js
+    */
+
+    const testContent = componentTestFileString({
+      componentName: formattedComponentName,
+    });
+    const testFileDescriptor = await open(
+      `${componentDirPath}/${formattedComponentName}.spec.js`,
+      "a"
+    );
+    await appendFile(testFileDescriptor, `${testContent}\n`);
+    await testFileDescriptor.close();
   } catch (error) {
     console.log("GLOBAL ERROR");
     console.log(error.message);
